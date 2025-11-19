8a4a1a462e278a0f8913769c6f0c4ff2a94ef2b8
Gabriella Shaka <gabsha28@gmail.com>
2025-11-19 09:52:12 -0500

    11/19/2025

--- Lesson4-Multiple Returns/lesson1.py
+++ Lesson4-Multiple Returns/lesson1.py
@@ -30,3 +30,38 @@ print(result == 0)  #True - numeric equality
 print(not result) #True - (falsey in boolean context)
 print(result is None) #False - different object 
 print(result is False) #False - different types
+
+# Multiple Returns - python packs multiple returns into a tuple!
+def calculate_room(length, width):
+    area = length * width
+    perimeter = 2 * (length + width)
+    return area, perimeter      #(Turns into a tuple)
+
+
+result = calculate_room(10, 5)
+print(result)
+print(type(result))
+
+print(type((42))) #int
+print(type((42,))) #tuple for single item
+no_parentheses = 1,2,3
+print(type((no_parentheses))) #tuple
+
+#unpacking tuple 
+area_result, perimeter_result = calculate_room(20,6)
+print(f"Area: {area_result}")
+print(f"Perimeter: {perimeter_result}")
+
+# Practice 1
+def analyze_grades(grades):
+    """Returns Avg, High, Low, Passed
+    if no grades, return, 0,0,0 False"""
+    if not grades:
+        return 0,0,0,False
+    average = sum(grades) / len(grades)
+    highest = max(grades)
+    lowest = min(grades)
+    passed = average >= 60
+    
+    return average, highest, lowest, True/False
+    
